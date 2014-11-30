17498b45b240d84d21738ee8ebf89ab0b39e83a7
Jason Fairbourn <jwfairbourn@gmail.com>
2014-11-29 17:29:45 -0700

    Adding Yeoman folders - Note that these do not use bootstrap, which may explain why it looks different than you are used to.  Re-running the generator using bootstrap gives the result shown in the example pages

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cubScoutTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cubScoutTrackerApp
+ */
+angular.module('cubScoutTrackerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
