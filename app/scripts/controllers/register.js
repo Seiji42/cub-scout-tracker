547ee65e9370fe6cb2ceb7b401e0757ffe36692a
Jason Fairbourn <jwfairbourn@gmail.com>
2014-12-01 20:51:39 -0700

    Added more functionality to the registration javascript

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -4,12 +4,17 @@ angular.module('cubScoutTrackerApp')
   .controller('RegisterCtrl', function ($scope) {
     $scope.username = "";
     $scope.password = "";
+    $scope.confirmPass = "";
     $scope.firstName = "";
     $scope.lastName = "";
     $scope.email = "";
-    $scope.packNum = 0;
+    $scope.packNum = "";
     $scope.isLeader = false;
     $scope.isParent = false;
+    $scope.leaderPhoneNumber = "";
+    $scope.leaderGroup = "";
+    $scope.selectedScouts = [];
+    //The following will eventually be obtained from the database
     $scope.wolfScouts = ["ed", "edd", "eddy"];
     $scope.bearScouts = ["foo", "bar"];
     $scope.webelosScouts = ["bill"];
@@ -22,6 +27,26 @@ angular.module('cubScoutTrackerApp')
     $scope.selectParent = function() {
         $scope.isParent = true;
         $scope.isLeader = false;
+    };
+
+    $scope.canSubmit = function() {
+        if($scope.username !== "" && $scope.password !== "" && $scope.firstName !== "" && $scope.lastName !== "" && 
+            $scope.email !== "" && $scope.packNum !== "" && $scope.confirmPass === $scope.password)
+        {
+            if($scope.isLeader && $scope.leaderPhoneNumber !== "" && $scope.leaderGroup !== "") //Can later add a clause to check for a valid pack
+                return true;
+            else if($scope.isParent && $scope.selectedScouts.length > 0)
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    };
+
+    $scope.submitInfo = function() {
+        console.log("Info submitted");
+        //this will later connect to the database and add the new information
     };
 
   });
